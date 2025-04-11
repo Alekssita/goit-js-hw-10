d2f044b304bbfffd9a75988b75c5703f4f674e13
Alekssita <113428804+Alekssita@users.noreply.github.com>
2025-04-11 12:42:12 +0200

    snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,44 @@
-console.log("Snackbar")
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const delay = Number(formData.get('delay'));
+  const state = formData.get('state');
+
+  createPromise(delay, state)
+    .then(ms => {
+      iziToast.success({
+        title: '✅ Fulfilled',
+        message: `Promise in ${ms}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    })
+    .catch(ms => {
+      iziToast.error({
+        title: '❌ Rejected',
+        message: `Promise in ${ms}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
